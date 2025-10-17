a49cb909904f190e952315eed99b950ab6a526b5
Kaifeng <kaifengp@gmail.com>
2025-10-17 15:27:58 -0400

    Changed BreedFetcher.BreedNotFoundException into a checked exception

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -34,7 +34,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         DogApiBreedFetcher breedFetcher = new DogApiBreedFetcher();
         String url = "https://dog.ceo/api/breed/" + breed + "/list";
         try {
